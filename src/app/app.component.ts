313d53aa88aede0212973ee601fb54b71ff8224d
Otis Chiemezie <otisidevelop@gmail.com>
2018-03-29 02:27:47 +0100

    updated app, added file media type checking before posting to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,23 @@ export class AppComponent implements OnInit {
   file: File;
   result: string;
   loading: boolean;
+  contentTypes: string[];
   // default constructor
   constructor(private service: AppService) {
     this.title = 'Audio 2 Text';
     this.result = '';
     this.loading = false;
+    this.contentTypes = [
+      'audio/basic',
+      'audio/flac',
+      'audio/l16',
+      'audio/mp3',
+      'audio/mpeg',
+      'audio/mulaw',
+      'audio/ogg',
+      'audio/webm'
+    ];
   }
-
 
   ngOnInit(): void {
     this.file = null;
@@ -26,28 +36,38 @@ export class AppComponent implements OnInit {
   // choose file
   onFile(fileInput: any) {
     this.file = fileInput.target.files[0];
+    if (!this.isFileVaild(this.file.type)) {
+      this.result = 'Media type not supported!';
+    } else {
+      this.result = '';
+    }
   }
 
   // send to server
   transcript() {
-
     if (this.file) {
-this.result = '';
+      this.result = '';
       // set loading to true
       this.loading = true;
       // post file to server
-      this.service.postFile(this.file)
-        .subscribe(item => {
+      this.service.postFile(this.file).subscribe(
+        item => {
           this.loading = false;
           this.result = item;
-        }, (err) => {
+        },
+        err => {
           this.loading = false;
           if (err._body) {
-             this.result = JSON.parse(err._body).message;
+            this.result = JSON.parse(err._body).message;
           }
-          this.result =  'An error occurred, please try again later.';
+          this.result = 'An error occurred, please try again later.';
           // console.log(JSON.stringify(JSON.parse(err._body), null, 2));
-        });
+        }
+      );
     }
+  }
+  isFileVaild(type): boolean {
+    const matched = this.contentTypes.filter(item => item === type);
+    return matched.length > 0;
   }
 }
